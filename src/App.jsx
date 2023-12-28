5282d5ead78afe4b2642cea9503f4477f64febbd
Danyvi <vinci.dani@gmail.com>
2023-12-28 23:15:22 +0100

    output data based on state

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react'
 import './App.css'
-import { CORE_CONCEPTS } from './data.js'
+import { CORE_CONCEPTS, EXAMPLES } from './data.js'
 import Header from './components/Header.jsx';
 import CoreConcept from './components/CoreConcept.jsx';
 import TabButton from './components/TabButton.jsx';
@@ -16,7 +16,7 @@ function App() {
   // it produces an array with 2 elements
   // const stateArray = useState('Please click a button');
   // using array destructuring
-  const [ selectedTopic, setSelectedTopic ] = useState('Please click a button');
+  const [ selectedTopic, setSelectedTopic ] = useState('components');
 
 
   function handleSelect(selectedButton) {
@@ -79,7 +79,16 @@ function App() {
               State
             </TabButton>
           </menu>
-          { selectedTopic }
+
+          <div id="tab-content">
+            <h3>{ EXAMPLES[selectedTopic].title }</h3>
+            <p>{ EXAMPLES[selectedTopic].description }</p>
+            <pre>
+              <code>
+                { EXAMPLES[selectedTopic].code }
+              </code>
+            </pre>
+          </div>
         </section>
       </main>
     </div>
